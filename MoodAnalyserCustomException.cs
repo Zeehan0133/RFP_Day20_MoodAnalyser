8eea675b983921d2899863e1759ac697c987a3e3
Zeehan0133 <zyne0133@gmail.com>
2021-10-11 16:12:55 +0530

    create moodanalyser coustom exception:

---
+++ MoodAnalyserCustomException.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyserCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            NO_SUCH_METHOD,
+            NO_SUCH_CLASS
+        }
+        private readonly ExceptionType type;
+        public MoodAnalyserCustomException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
